ca0408170d4da9172176a431c64cc1c33f82cf21
JUniverse <106907963+jamaluniverse@users.noreply.github.com>
2023-05-26 14:49:26 +0700

    myScript.js

--- myScript.js
+++ myScript.js
@@ -4,3 +4,8 @@ document.addEventListener("copy", (evt) => {
     evt.clipboardData.setData("text/plain", "menyalin tidak diperkenankan pada wesbite saya");
     evt.preventDefault();
     }, false);
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
